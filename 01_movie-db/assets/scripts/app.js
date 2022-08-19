21a12197711b7d6a6a7fbaec3a4c3abada945e61
Highlander Magno <highlander9200@gmail.com>
2022-08-19 10:26:03 -0300

    Rearranged the order of some constants, renamed functions and added basic user input validation to the modal

--- 01_movie-db/assets/scripts/app.js
+++ 01_movie-db/assets/scripts/app.js
@@ -1,22 +1,39 @@
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
-
 const backDrop = document.getElementById("backdrop");
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
 
 const toggleBackdrop = () => {
   backDrop.classList.toggle("visible");
 };
-
-const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle("visible");
   toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
   toggleMovieModal();
-}
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imgUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imgUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please enter valid values (rating between 1 and 5).");
+    return;
+  }
+};
 
 const backDropClickHandler = () => {
   toggleMovieModal();
@@ -24,4 +41,5 @@ const backDropClickHandler = () => {
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
 backDrop.addEventListener("click", backDropClickHandler);
-cancelAddMovieButton.addEventListener("click", cancelAddMovie);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
