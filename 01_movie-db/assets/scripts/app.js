c49c0649c3031c857898232fbfce186bba127ea6
Highlander Magno <highlander9200@gmail.com>
2022-08-19 09:29:50 -0300

    :sparkles: Added option to open/close modal on click

--- 01_movie-db/assets/scripts/app.js
+++ 01_movie-db/assets/scripts/app.js
@@ -1,8 +1,27 @@
-const addMovieModal = document.body.children[1];
+const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
+
+const backDrop = document.getElementById("backdrop");
+
+const toggleBackdrop = () => {
+  backDrop.classList.toggle("visible");
+};
+
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+}
+
+const backDropClickHandler = () => {
+  toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
+backDrop.addEventListener("click", backDropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovie);
